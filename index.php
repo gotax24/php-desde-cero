80f39f32ae79b16f115c0f397e45f0b90037c8ba
Ernesto Bracho <dev.ejbr@gmail.com>
2026-03-10 16:52:09 -0400

    optimización del Dockerfile y mejoras en el archivo index.php

--- index.php
+++ index.php
@@ -1,8 +1,111 @@
 <?php
-
-$name = 'Yal';
 //Para concatenar string es con un .
-echo "Hola mundo soy " . $name;
+
+//tipos de datos
+$name = 'Yal'; //string 
+$quantity = 14; //integer
+$price = 9.99; //float
+$paymentStatus = true; //bool
+$startDate    =   null; //null
 
 //para escribir variables dentro de un string
 echo "Hola {$name}";
+
+echo "  Hola mundo soy " . $name . "  ";
+var_dump($quantity);
+var_dump($name);
+
+$taskCompleted = true;
+
+if ($taskCompleted) {
+  echo "Tarea completada";
+} else {
+  echo "Tarea incompleta";
+}
+
+$price = 10.50;
+
+if ($price > 10) {
+  echo 'No Comprar';
+} else {
+  echo 'Comprar';
+}
+
+$names = [
+  'Luis',
+  'Jesus',
+  'Ernesto',
+  'Pedro',
+];
+
+$ages = [
+  22,
+  18,
+  17,
+  20,
+];
+
+//array asociativos
+$players = [
+  'portero' => 'Bravo',
+  'Goat' => 'Messi',
+  'Entrenador' => 'Ernesto Bracho'
+];
+
+$players2 = [
+  'Porteros' => [
+    'Portero 1',
+    'Portero 2',
+    'Portero 3',
+  ],
+  'Goat' => 'Messi'
+];
+
+
+echo $players['Goat'];
+echo $players2['Porteros'][2]; //tambien se puede hacer al reves
+
+$itemsNumber = count($names);
+
+for ($i = 0; $i < $itemsNumber; $i++) {
+  echo $i . " ";
+  echo $names[$i] . "<br>";
+}
+
+//primero el array 
+//segundo es el indice
+//tercero el valor del items
+foreach ($names as $key => $nam) {
+  echo $nam;
+}
+
+//si no se usa el index 
+foreach ($ages as $value) {
+  echo $value;
+}
+
+function dd($value) {
+  return die(var_dump($value));
+}
+
+dd($name);
+
+$tasks = [
+  [
+    'title' => 'Hola',
+    'task' => true
+  ],
+  [
+    'title' => 'perra',
+    'task' => false
+  ]
+];
+
+$
+
+foreach($tasks as $tarea){
+  echo $tarea['title'] . ($tarea['task'] ? 'Si'  : 'No') . "<br>";
+}
+
+//muestra el mensaje y termina el script ahi no sigue avanzando
+die('hola');
